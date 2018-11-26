c7d8844351cc307d9e414a81953aae8be78ed7e7
unknown <765615591@qq.com>
2018-11-26 15:54:53 +0800

    add detail

---
+++ src/components/Gallary.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="gallery">
+    <div class="swiper-container" 
+      @click="handleGallaryClick"
+    >
+      <div class="swiper-wrapper">
+        <swiper :options="swiperOption" >
+          <swiper-slide 
+            v-for="(item, index) of gallaryImgs"
+            :key="index"
+          >
+            <img class="swiper-img" :src="item" />
+          </swiper-slide>
+          <div class="swiper-pagination" slot="pagination"></div>
+        </swiper>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailGallery',
+  props: {
+    gallaryImgs: Array
+  },
+  data () {
+    return {
+      isShowSwpier: false,
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        paginationType: 'fraction',
+        observer:true,
+        observeParents:true,
+      }
+    }
+  },
+  methods: {
+    handleGallaryClick () {
+      //this.isShowSwpier = true
+      this.$emit('close')
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .gallery >>> .swiper-pagination
+    color: #fff
+  .gallery
+    .gallery-container
+      position: fixed
+      top: 0
+      left: 0
+      right: 0
+      height: 0
+      padding-bottom: 55%
+      .gallery-img
+        width: 100%
+      .gallery-info
+        position: absolute
+        left: 0
+        right: 0
+        bottom: 0
+        height: .76rem
+        display: flex
+        background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, .5))
+        .gallery-text
+          flex: 1
+          padding-left: .2rem
+          line-height: .76rem
+          font-size: .32rem
+          color: #fff
+        .gallery-count
+          margin-top: .2rem
+          margin-right: .1rem
+          width: 1rem
+          height: .44rem
+          color: #fff
+          text-align: center
+          border-radius: .4rem
+          background: rgba(0, 0, 0, .7)
+    .swiper-container
+      background: #000
+      position: fixed
+      top: 0
+      left: 0
+      right: 0
+      bottom: 0
+      z-index: 3
+      display: flex
+      flex-direction: column
+      justify-content: center
+      .swiper-wrapper
+        width: 100%
+        height: 0
+        padding-bottom: 100%
+        .swiper-img
+          width: 100%
+
+</style>
--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,9 +1,78 @@
 <template>
-  <div>Detail</div>
+  <div class="detail">
+    <detail-header></detail-header>
+    <detail-banner 
+      :sightName="sightName"
+      :bannerImg="bannerImg"
+      :gallaryLen="getGallaryLen"
+      @show="handleGallaryShow"
+    >
+    </detail-banner>
+    <gallary 
+      :gallaryImgs="gallaryImgs" 
+      v-if="isShowSwpier"
+      @close="handleGallaryClose"
+    ></gallary>
+    <detail-list></detail-list>
+    {{this.$route.params.id}}
+  </div>
 </template>
 
 <script>
+import DetailHeader from '@/views/detail/components/DetailHeader'
+import DetailBanner from '@/views/detail/components/DetailBanner'
+import Gallary from '@/components/Gallary'
+import DetailList from '@/views/detail/components/DetailList'
+import axios from 'axios'
 export default {
-  name: 'Detail'
+  name: 'Detail',
+  components: {
+    DetailHeader,
+    DetailBanner,
+    Gallary,
+    DetailList
+  },
+  data () {
+    return {
+      sightName: '',
+      bannerImg: '',
+      gallaryImgs: [],
+      isShowSwpier: false
+    }
+  },
+  mounted () {
+    this.getDetailData()
+  },
+  computed: {
+    getGallaryLen () {
+      console.log(this.gallaryImgs)
+      return this.gallaryImgs.length
+    }
+  },
+  methods: {
+    getDetailData () {
+      axios.get('/api/detail.json')
+        .then(res => {
+          this.setDetailData(res)}
+          )
+        .catch(e => {
+          console.log(e)
+        })
+    },
+    setDetailData (res) {
+      if(res.data && res.data.ret) {
+        const detailData = res.data.data
+        this.sightName = detailData.sightName
+        this.bannerImg = detailData.bannerImg
+        this.gallaryImgs = detailData.gallaryImgs
+      }
+    },
+    handleGallaryShow () {
+      this.isShowSwpier = true
+    },
+    handleGallaryClose () {
+      this.isShowSwpier = false
+    }
+  }
 }
 </script>
